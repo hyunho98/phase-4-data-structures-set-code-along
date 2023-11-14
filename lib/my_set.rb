7510ca0177203197bc2841ce6b139afa24c56db4
Alex Lee <hyunho98@gmail.com>
2023-11-13 22:13:05 -0500

    commit tree c24ba0ea73494a3813e8b2ae2d63addc11999df8

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,48 @@
 # your code here
+class MySet
+    def initialize(enumerable = [])
+        @hash = {}
+        enumerable.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def size
+        @hash.size
+    end
+
+    def self.[](*enumerable)
+        new(enumerable)
+    end
+
+    def clear
+        @hash.clear
+        self
+    end
+
+    def each
+        for element in @hash.keys
+            yield element
+        end
+        self
+    end
+    
+    def inspect
+        keys = @hash.keys
+        "#<MySet: {#{keys.join(', ')}}>"
+    end
+end
